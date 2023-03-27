60444061232e002eeeb10401b53facee9bdb4e04
Mike McGrail <mike.mcgrail@sentinelone.com>
2023-03-27 18:32:35 -0400

    use common functions

--- package/bin/dataset_powerquery.py
+++ package/bin/dataset_powerquery.py
@@ -9,10 +9,12 @@ import traceback
 import math
 
 from dataset_common import *
+from dataset_api import *
 
 import import_declare_test
 from splunklib import modularinput as smi
 from solnlib import conf_manager, log
+from solnlib.modular_input import checkpointer
 
 APP_NAME = __file__.split(op.sep)[-3]
 
@@ -32,6 +34,11 @@ class DATASET_POWERQUERY_INPUT(smi.Script):
         scheme.add_argument(smi.Argument("name", title="Name",
                                          description="",
                                          required_on_create=True))
+        
+        scheme.add_argument(smi.Argument("account", title="Account",
+                                         description="DataSet account",
+                                         required_on_create=True,
+                                         required_on_edit=True))
 
         scheme.add_argument(smi.Argument("start_time", title="Start Time",
                                          description="Relative start time to query back. Use short form relative time, e.g.: 24h or 30d. Reference https://app.scalyr.com/help/time-reference",
@@ -74,99 +81,76 @@ class DATASET_POWERQUERY_INPUT(smi.Script):
 
         # Input logic here
         try:
-            #Create checkpointer
-            checkpoint = checkpointer.KVStoreCheckpointer(
-                input_name,
-                session_key,
-                APP_NAME
-            )
-            ds_start_time = input_items.get('start_time')
+            ds_account = input_items.get('account')
+            ds_start = input_items.get('start_time')
+            ds_end = input_items.get('end_time')
+            ds_search = input_items.get('dataset_query_string')
+            ds_payload = build_payload(ds_start, ds_end, 'powerquery', ds_search)
+            proxy = get_proxy(session_key, logger)
+            acct_dict = get_acct_info(self, logger, ds_account)
+            for ds_acct in acct_dict.keys():
+                ds_url = get_url(acct_dict[ds_acct]['base_url'], 'powerquery')
+                ds_headers = { "Authorization": "Bearer " + acct_dict[ds_acct]['ds_api_key'] }
 
-            ds_environment = get_environment(session_key, logger)
-            ds_url = get_url(ds_environment, 'powerQuery')
-            ds_api_key = get_token(session_key, logger, 'read')
-            ds_headers = { "Authorization": "Bearer " + ds_api_key }
-            ds_proxy = get_proxy(session_key, logger)
+                #Create checkpointer
+                checkpoint = checkpointer.KVStoreCheckpointer(
+                    input_name,
+                    session_key,
+                    APP_NAME
+                )
 
-            ds_payload = { "startTime": ds_start_time }
+                logger.debug("DataSetFunction=sendRequest, destination={}, startTime={}".format(ds_url, time.time()))
+                r = requests.post(url=ds_url, headers=ds_headers, json=ds_payload, proxies=proxy)
+                r_json = r.json() #parse results json
 
-            ds_query = input_items.get('dataset_query_string')
-            ds_payload['query'] = ds_query
-
-            ds_end_time = input_items.get('end_time')
-            if ds_end_time:
-                ds_payload['endTime'] = ds_end_time        
-
-            r = requests.post(url=ds_url, headers=ds_headers, json=ds_payload, proxies=ds_proxy)
-            r_json = r.json() #parse results json
-
-            #first, validate success
-            if r.ok:
-                #log information from results
-                if 'status' in r_json:
-                    logger.info("response status=%s" % str(r_json['status']))
-                
-                if 'warnings' in r_json:
-                    for warning in r_json['warnings']:
-                        logger.warning("response warning=%s" % str(warning))
+                #first, validate success
+                if r.ok:
+                    #log information from results
+                    if 'status' in r_json:
+                        logger.info("response status={}".format(r_json['status']))
                     
-                if 'matchingEvents' in r_json:
-                    logger.info("response matches=%s" % str(r_json['matchingEvents']))
-                    
-                if 'cpuUsage' in r_json:
-                    logger.info('cpuUsage: %s ' % r_json['cpuUsage'] )
+                    if 'warnings' in r_json:
+                        for warning in r_json['warnings']:
+                            logger.warning("response warning={}".format(warning))
+                        
+                    if 'matchingEvents' in r_json:
+                        logger.info("response matches={}".format(r_json['matchingEvents']))
+                        
+                    if 'cpuUsage' in r_json:
+                        logger.info('cpuUsage={}'.format(r_json['cpuUsage']))
 
-                #parse results, match returned columns with corresponding values
-                if 'columns' in r_json and 'values' in r_json:
-                    for value_list in r_json['values']:
-                        ds_event_dict = {}
+                    #parse results, match returned columns with corresponding values
+                    if 'columns' in r_json and 'values' in r_json:
+                        for value_list in r_json['values']:
+                            ds_event, splunk_dt = parse_powerquery(value_list, r_json['columns'])
+                            get_checkpoint = checkpoint.get(input_name)
 
-                        for counter in range(len(value_list)):
-                            ds_event_dict[r_json['columns'][counter]['name']] = value_list[counter]
-                    
-                        #check event time against checkpoint
-                        if 'timestamp' in ds_event_dict:
-                            event_time = int(ds_event_dict['timestamp'])
-                        else:
-                            #if no timestamp, use current time in nanoseconds
-                            event_time = int(time.time()) * 1000000000
+                            #if checkpoint doesn't exist, set to 0
+                            if get_checkpoint == None:
+                                checkpoint.update(input_name, {"timestamp": 0})
+                                checkpoint_time = 0
+                            else:
+                                checkpoint_time = float(get_checkpoint["timestamp"])
 
-                        get_checkpoint = checkpoint.get(input_name)
+                            if splunk_dt > checkpoint_time:
+                                #if greater than current checkpoint, write event and update checkpoint
+                                event = smi.Event(
+                                    stanza=input_name,
+                                    data=json.dumps(ds_event),
+                                    source=input_name,
+                                    sourcetype='dataset:powerquery',
+                                    time=splunk_dt
+                                )
+                                logger.debug("writing event with splunk_dt={}, checkpoint={}".format(splunk_dt,checkpoint_time))
+                                ew.write_event(event)
 
-                        #if checkpoint doesn't exist, set to 0
-                        if get_checkpoint == None:
-                            checkpoint.update(input_name, {"timestamp": 0})
-                            checkpoint_time = 0
-                        else:
-                            checkpoint_time = int(get_checkpoint["timestamp"])
+                                logger.debug("saving checkpoint {}".format(splunk_dt))
+                                checkpoint.update(input_name, {"timestamp": splunk_dt})
+                            else:
+                                logger.debug("skipping due to splunk_dt={} is less than checkpoint={}".format(splunk_dt, checkpoint_time))
 
-                        if event_time > checkpoint_time:
-                            #if greater than current checkpoint, write event and update checkpoint
-                            splunk_dt = normalize_time(int(event_time))
-                            ds_event = json.dumps(ds_event_dict)
-                            #create and write event
-                            event = smi.Event(
-                                stanza=input_name,
-                                data=ds_event,
-                                source=input_name,
-                                sourcetype='dataset:powerquery',
-                                time=splunk_dt
-                            )
-                            logger.debug("writing event with event_time=%s and checkpoint=%s" % (str(event_time), str(checkpoint_time)))
-                            ew.write_event(event)
-
-                            logger.debug("saving checkpoint %s" % (str(event_time)))
-                            checkpoint.update(input_name, {"timestamp": event_time})
-                        else:
-                            logger.debug("skipping due to event_time=%s is less than checkpoint=%s" % (str(event_time), str(checkpoint_time)))
-                else:
-                    logger.info("no matching events")
-
-            else:
-                if 'message' in r_json:
-                    logger.error(r_json['message'])
-                else:
-                    logger.error("response = {}".format(r_json))
+                    else: #if no resulting ['values'] and ['columns']
+                        logger.warning('DataSet response success, no matches returned')
 
         except Exception as e:
             logger.exception(e)
