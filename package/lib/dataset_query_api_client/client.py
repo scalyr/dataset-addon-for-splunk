a95b5faf9cfaf8118249827d88f409967c6d94e1
Mike McGrail <mike.mcgrail@sentinelone.com>
2023-04-21 15:40:44 -0400

    add user-agent

--- package/lib/dataset_query_api_client/client.py
+++ package/lib/dataset_query_api_client/client.py
@@ -63,4 +63,5 @@ class AuthenticatedClient(Client):
     def get_headers(self) -> Dict[str, str]:
         """Get headers to be used in authenticated endpoints"""
         auth_header_value = f"{self.prefix} {self.token}" if self.prefix else self.token
-        return {self.auth_header_name: auth_header_value, **self.headers}
+
+        return {self.auth_header_name: auth_header_value, "User-Agent": "splunk-ta", **self.headers}
