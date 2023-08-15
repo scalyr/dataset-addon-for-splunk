d8213c3f49033e2f1d639e9016a556ecb8700d40
Martin Majlis <martin.majlis@sentinelone.com>
2023-08-15 15:46:19 +0200

    Remove dummy failing test

--- tests/test_dataset_common.py
+++ tests/test_dataset_common.py
@@ -6,7 +6,3 @@ def test_normalize_time():
     dataset_time_nanos = int(1692099758000000000)
     splunk_time_seconds = normalize_time(dataset_time_nanos)
     assert splunk_time_seconds == int(1692099758)
-
-
-def test_should_fail():
-    assert 3 == 5
