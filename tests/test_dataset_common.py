c0bd27bc781eb5dfdef200fef3bafadbb34320e3
Martin Majlis <122797378+martin-majlis-s1@users.noreply.github.com>
2023-08-15 15:54:33 +0200

    DPDV-2402: Use pytest (#64)
    * DPDV-2402 test: introduce GitHub action unittest workflow
    
    * DPDV-2402 test: introduce GitHub action unittest workflow
    
    * DPDV-2402: Use pytest to write tests
    
    * Install dependencies
    
    * Remove dummy failing test
    
    ---------
    
    Co-authored-by: tomas.zdara <tomas.zdara@sentinelone.com>

---
+++ tests/test_dataset_common.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from dataset_common import normalize_time
+
+
+def test_normalize_time():
+    dataset_time_nanos = int(1692099758000000000)
+    splunk_time_seconds = normalize_time(dataset_time_nanos)
+    assert splunk_time_seconds == int(1692099758)
