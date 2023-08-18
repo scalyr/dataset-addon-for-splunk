49f32117e28690e319d48b58e68549c5e8a1bf5c
Martin Majlis <122797378+martin-majlis-s1@users.noreply.github.com>
2023-08-18 12:07:16 +0200

    DPDV-2719: Fix failing tests (#67)
    * Fix failing tests
    
    * Add comment explaining why there is a condition

--- e2e/utils.ts
+++ e2e/utils.ts
@@ -1,4 +1,4 @@
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 import { test, expect } from '@playwright/test';
 
 export async function goToDataSet(page: Page) {
@@ -22,22 +22,37 @@ export async function goToSearch(page: Page) {
 }
 
 export async function waitForData(page: Page, key: string) {
-    // Wait for the "Waiting for data..." text to disappear
+    await page.waitForTimeout(5000);
+
+    let pic = 0
+
+    await page.screenshot({ path: `playwright-screenshots/page-${ key }-wait-for-data-${ pic++ }.png`, fullPage: true });
+
+    console.log("Wait for 'Waiting for queued job to start' to disappear")
+    await expect(page.getByText(/Waiting for queued job to start/)).toHaveCount(0, {timeout: 50000})
+    await page.screenshot({ path: `playwright-screenshots/page-${ key }-wait-for-data-${ pic++ }.png`, fullPage: true });
+
     console.log("Wait for 'Waiting for data' to disappear")
-    await page.screenshot({ path: `playwright-screenshots/page-${ key }-before-waiting-for-data.png`, fullPage: true });
-    await expect(page.getByText(/Waiting for data/)).toHaveCount(0, {timeout: 15000});
+    await expect(page.getByText(/Waiting for data/)).toHaveCount(0, {timeout: 50000})
+    await page.screenshot({ path: `playwright-screenshots/page-${ key }-wait-for-data-${ pic++ }.png`, fullPage: true });
 
-    await page.screenshot({ path: `playwright-screenshots/page-${ key }-before-checks.png`, fullPage: true });
+    console.log("Wait for 'No results yet found' to disappear")
+    await expect(page.getByText(/No results yet found/)).toHaveCount(0, {timeout: 50000})
+    await page.screenshot({ path: `playwright-screenshots/page-${ key }-wait-for-data-${ pic++ }.png`, fullPage: true });
 
-    // Check if the page does not contain the text "No results found."
-    const noResultsCount = await page.getByText(/No results found/).count();
-    console.log("Page contains 'No results found': ", noResultsCount)
-    expect(noResultsCount).toBe(0);
+    // It looks that No results found element is still there, but not visible
+    console.log("Wait for 'No results found' to disappear");
+    const locNoResults = page.getByText(/No results found/);
+    if (await locNoResults.count() > 0) {
+        if (await locNoResults.isVisible()) {
+            await expect(locNoResults).toHaveCount(0, {timeout: 50000})
+            await page.screenshot({ path: `playwright-screenshots/page-${ key }-wait-for-data-${ pic++ }.png`, fullPage: true });
+        }
+    }
 
-    // Check if the page does not contain the text "No results found."
-    const searchFailedCount = await page.getByText(/External search command exited unexpectedly/).count();
-    console.log("Page contains 'External search command exited unexpectedly': ", searchFailedCount)
-    expect(searchFailedCount).toBe(0);
+    console.log("Page contains 'External search command exited unexpectedly'")
+    await expect(page.getByText(/External search command exited unexpectedly/)).toHaveCount(0, {timeout: 50000});
+    await page.screenshot({ path: `playwright-screenshots/page-${ key }-wait-for-data-${ pic++ }.png`, fullPage: true });
 
     const { WAIT_FOR_HUMAN_TO_CHECK_IN_MS: waitForHumanStr} = process.env
     const waitForHumanMs = parseInt(waitForHumanStr || '0');
