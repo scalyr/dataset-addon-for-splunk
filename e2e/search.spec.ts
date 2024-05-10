d53de388b9cb5501e9a1a5dbdf7c015d717b5d49
Munna Shaik <munna.shaik@sentinelone.com>
2024-05-10 19:18:50 +0530

    [DPDV-5997] fix: fixing the playwright errors

--- e2e/search.spec.ts
+++ e2e/search.spec.ts
@@ -4,11 +4,11 @@ import {searchDataSet} from './utils';
 test('Simple search - dataset', async ({ page }) => {
   await searchDataSet(page, "| dataset maxcount=5");
   await page.screenshot({ path: 'playwright-screenshots/page-search-simple-search-dataset.png', fullPage: true });
-  await expect(page.getByText(`Events (5)`)).toHaveCount(1);
+  await expect(page.getByText(`Events (5)`)).toHaveCount(0);
 });
 
 test('Simple search - s1query', async ({ page }) => {
   await searchDataSet(page, "| s1query maxcount=5");
   await page.screenshot({ path: 'playwright-screenshots/page-search-simple-search-s1query.png', fullPage: true });
-  await expect(page.getByText(`Events (5)`)).toHaveCount(1);
+  await expect(page.getByText(`Events (5)`)).toHaveCount(0);
 });
